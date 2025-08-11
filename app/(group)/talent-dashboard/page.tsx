c4c1457bc2253fe14e3d710269aa51cb27516661
Rishi Patel <rishipatel2552@gmail.com>
2025-08-10 23:40:54 -0400

    fix(dashboards): resolve minor frontend issue

--- app/(group)/talent-dashboard/page.tsx
+++ app/(group)/talent-dashboard/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 import * as React from "react"
+import { useEffect, useState } from "react"
 import { SVGProps } from "react"
+import { useAuth } from "@/contexts/auth"
 import AuthenticatedRoute from "@/hoc/AuthenticatedRoute"
 import data from "@/public/mock/projects.json"
+import SecuritySettingsStepper from "@/stories/security-settings.stories"
 import { ONE_SECOND } from "@/utils/constants"
 import { cn, getId, getIsEmpty, getIsNotEmpty } from "@/utils/functions"
 import { useEditable } from "@ark-ui/react"
@@ -1930,71 +1933,97 @@ const Footer = () => {
 
 export default function TalentDashboard() {
   const [isOpen, toggleIsOpen] = useToggle(false)
+  const { user } = useAuth()
+  const [showSecurityModal, setShowSecurityModal] = useState(false)
+
+  useEffect(() => {
+    const safeUser = user as {
+      provider?: string
+      password?: string
+    }
+
+    const passwordIsEmpty =
+      typeof safeUser?.password !== "string" ||
+      safeUser.password.trim().length === 0
+
+    if (safeUser && safeUser.provider === "EMAIL" && passwordIsEmpty) {
+      setShowSecurityModal(true)
+    }
+  }, [user])
 
   return (
-    <AuthenticatedRoute>
-      <div className="flex bg-gray-50">
-        <LeftSidebar />
-        <div className="flex-auto p-6 gap-x-8 flex justify-center min-[1024px]:py-8 min-[1024px]:pl-[256px] min-[1024px]:pr-8 xl:pr-[50px] xl:pl-[274px] 2xl:pr-[150px] 2xl:pl-[374px] overflow-hidden">
-          <div className="max-w-[1042px] space-y-3 md:space-y-5 min-w-0">
-            <Welcome onToggle={toggleIsOpen} />
-
-            <RecentProjects />
-
-            <CreateYourOwnProject />
-
-            <RecommendedForYou />
-
-            <BecomeTopSeller />
-
-            <Carousel />
-
-            <TakeCareersToNextLevel />
-
-            <Footer />
-          </div>
-
-          <RightSidebar />
+    <>
+      {showSecurityModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+          <SecuritySettingsStepper
+            onCloseModal={() => setShowSecurityModal(false)}
+          />
         </div>
+      )}
+      <AuthenticatedRoute>
+        <div className="flex bg-gray-50">
+          <LeftSidebar />
+          <div className="flex-auto p-6 gap-x-8 flex justify-center min-[1024px]:py-8 min-[1024px]:pl-[256px] min-[1024px]:pr-8 xl:pr-[50px] xl:pl-[274px] 2xl:pr-[150px] 2xl:pl-[374px] overflow-hidden">
+            <div className="max-w-[1042px] space-y-3 md:space-y-5 min-w-0">
+              <Welcome onToggle={toggleIsOpen} />
 
-        <Dialog open={isOpen} onOpenChange={toggleIsOpen}>
-          <DialogContent
-            variant="unanimated"
-            className="w-full md:w-[322px] bg-white data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:slide-in-from-right-1/2 data-[state=closed]:slide-out-to-right-1/2 inset-x-0 md:left-auto md:right-0 inset-y-0"
-          >
-            <button
-              className="size-[28px] -left-3.5 absolute z-10 top-[104px] text-gray-500 focus-visible:outline-none rounded-full bg-white hidden md:inline-flex border border-gray-300 items-center justify-center shrink-0"
-              onClick={toggleIsOpen}
-            >
-              <ChevronRight className="size-3" />
-            </button>
+              <RecentProjects />
 
-            <div className="pl-3.5 p-1 sticky top-0 h-12 flex md:hidden items-center justify-between">
-              <h1 className="text-sm leading-5 font-semibold text-dark-blue-400">
-                My Profile
-              </h1>
+              <CreateYourOwnProject />
 
-              <DialogTrigger asChild>
-                <IconButton
-                  className="opacity-50 hover:opacity-100"
-                  size="md"
-                  variant="ghost"
-                  visual="gray"
-                >
-                  <X className="size-[18px]" />
-                </IconButton>
-              </DialogTrigger>
+              <RecommendedForYou />
+
+              <BecomeTopSeller />
+
+              <Carousel />
+
+              <TakeCareersToNextLevel />
+
+              <Footer />
             </div>
 
-            <ScrollArea
-              className="w-full h-[calc(theme(size.full)-theme(size.12))]"
-              scrollBar={<ScrollBar className="w-4 p-1" />}
+            <RightSidebar />
+          </div>
+
+          <Dialog open={isOpen} onOpenChange={toggleIsOpen}>
+            <DialogContent
+              variant="unanimated"
+              className="w-full md:w-[322px] bg-white data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:slide-in-from-right-1/2 data-[state=closed]:slide-out-to-right-1/2 inset-x-0 md:left-auto md:right-0 inset-y-0"
             >
-              <RightDrawer />
-            </ScrollArea>
-          </DialogContent>
-        </Dialog>
-      </div>
-    </AuthenticatedRoute>
+              <button
+                className="size-[28px] -left-3.5 absolute z-10 top-[104px] text-gray-500 focus-visible:outline-none rounded-full bg-white hidden md:inline-flex border border-gray-300 items-center justify-center shrink-0"
+                onClick={toggleIsOpen}
+              >
+                <ChevronRight className="size-3" />
+              </button>
+
+              <div className="pl-3.5 p-1 sticky top-0 h-12 flex md:hidden items-center justify-between">
+                <h1 className="text-sm leading-5 font-semibold text-dark-blue-400">
+                  My Profile
+                </h1>
+
+                <DialogTrigger asChild>
+                  <IconButton
+                    className="opacity-50 hover:opacity-100"
+                    size="md"
+                    variant="ghost"
+                    visual="gray"
+                  >
+                    <X className="size-[18px]" />
+                  </IconButton>
+                </DialogTrigger>
+              </div>
+
+              <ScrollArea
+                className="w-full h-[calc(theme(size.full)-theme(size.12))]"
+                scrollBar={<ScrollBar className="w-4 p-1" />}
+              >
+                <RightDrawer />
+              </ScrollArea>
+            </DialogContent>
+          </Dialog>
+        </div>
+      </AuthenticatedRoute>
+    </>
   )
 }
